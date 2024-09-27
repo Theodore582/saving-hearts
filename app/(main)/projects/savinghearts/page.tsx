b9cbce497001793358d375a81efaeadeb49af2a6
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-09-27 06:38:00 +0000

    fix: deployment error apos

--- app/(main)/projects/savinghearts/page.tsx
+++ app/(main)/projects/savinghearts/page.tsx
@@ -19,7 +19,7 @@ const page = () => {
                 <section>
 
                 <div className='font-raleway text-center'>
-                    <h1 className='text-primaryy font-semibold text-lg pt-5'>WHAT WE'VE DONE</h1>
+                    <h1 className='text-primaryy font-semibold text-lg pt-5'>WHAT WE&apos;VE DONE</h1>
                     <h1 className=' font-raleway m-auto text-3xl lg:text-5xl font-semibold lg:w-[600px] lg:leading-[60px] text-black'>THE SAVING HEARTS STORY SO FAR IN PICTURES</h1>
                 </div>
                 <div>
